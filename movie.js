ff28d4aa774baaf6f4d1ca42e386567091df8375
bestauffer <77029920+bestauffer@users.noreply.github.com>
2021-02-02 10:28:42 -0800

    Add files via upload

--- movie.js
+++ movie.js
@@ -8,4 +8,14 @@ function MovieValidate(){
 let myMovie = new Movie();
 myMovie.movieTitle = document.getElementById("title").value;
 myMovie.movieRating = document.getElementById("rating").value;
+
+if(myMovie.movieTitle = ""){
+alert("Movie title cannot be blank")
+}
+else if(myMovie.movieRating < 1 || myMovie.movieRating > 5 || myMovie.movieRating == ""){
+alert("you must enter a rating that is between 1-5")
+}
+else{
+    return myMovie.ToString();
+}
 }
